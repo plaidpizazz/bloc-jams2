fa80e67a9bb9b591b78dd9af220b978b74ac8a60
plaidpizazz <marcellus.spears@gmail.com>
2017-02-09 12:44:30 -0600

    Assignment 8 update 2 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,11 +1,15 @@
-var revealPoint = function () {
+var animatePoints = function (){
 
     var points = document.getElementsByClassName('point');
 
-        for (var i = 0; i < points.length; i++) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
+        var revealPoint = function(index){
+            points[index].style.opacity = 1;
+            points[index].style.transform = "scaleX(1) translateY(0)";
+            points[index].style.msTransform = "scaleX(1) translateY(0)";
+            points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+        };
+    var length = points.length
+    for (var i = 0; i < length; i++){
+        revealPoint(i);
+    }
 };
