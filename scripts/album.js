041ba4cb0f485d0fd205bacfd5f0afed5cf434ab
plaidpizazz <marcellus.spears@gmail.com>
2017-03-01 16:21:33 -0600

    Checkpoint 19 next: completed

--- scripts/album.js
+++ scripts/album.js
@@ -11,12 +11,13 @@ var createSongRow = function(songNumber, songName, songLength) {
     
     var clickHandler = function() { 
     
-        var songNumber = $(this).attr('.data-song-number');
+        var songNumber = parseInt($(this).attr('.data-song-number'));
 
         if (currentlyPlayingSongNumber !== null) {
             var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
             currentlyPlayingCell.html(currentlyPlayingSongNumber);
-        } 
+        }
+        
         if (currentlyPlayingSongNumber !== songNumber) {
             $(this).html(pauseButtonTemplate);
             currentlyPlayingSongNumber = songNumber;
@@ -33,7 +34,7 @@ var createSongRow = function(songNumber, songName, songLength) {
     
     var onHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
-        var songNumber = songNumberCell.attr('data-song-number');
+        var songNumber = parseInt(songNumberCell.attr('data-song-number'));
         
         if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(playButtonTemplate);
@@ -42,7 +43,7 @@ var createSongRow = function(songNumber, songName, songLength) {
     
     var offHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
-        var songNumber = songNumberCell.attr('data-song-number');
+        var songNumber = parseInt(songNumberCell.attr('data-song-number'));
         
         if (songNumber !== currentlyPlayingSongNumber) {
             songNumberCell.html(songNumber);
